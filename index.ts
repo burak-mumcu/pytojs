c3a49f45d8d75c2cc6453e8e30e53bd48941c155
Burak Mumcu <95939910+burak-mumcu@users.noreply.github.com>
2024-03-09 16:36:34 +0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -39,11 +39,3 @@ export const pyProcess = async (path: string, args?: any[]) => {
     });
 };
 
-(async () => {
-    try {
-        const result = await pyProcess('./deneme.py',['gelmez dimi ya']);
-        console.log(result);
-    } catch (error) {
-        console.error('Hata:', error);
-    }
-})();
