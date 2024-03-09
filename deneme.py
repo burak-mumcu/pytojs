4fa049aada9a261881cc124b60c9aef2e0708a74
Burak Mumcu <95939910+burak-mumcu@users.noreply.github.com>
2024-03-09 15:23:10 +0300

    fix:code improving and fixind

---
+++ deneme.py
@@ -0,0 +1,6 @@
+def main():
+    return '1002'
+
+if __name__ == "__main__":
+    print(main())
+
--- index.ts
+++ index.ts
@@ -1,24 +1,48 @@
 import { spawn } from 'child_process';
 
-export const pyProcess = (path:string, args?:any[]):string => {
-    const process = args ? spawn('python', [path,...args]) : spawn('python', [path])
-    let stringData = ''
-    process.stdout.on('data', (data) => {
-        stringData += data.toString();
+export const pyProcess = async (path: string, args?: any[]) => {
+    return new Promise<string>((resolve, reject) => {
+        const process = args ? spawn('python', [path, ...args]) : spawn('python', [path])
+        let stringData = ''
+
+        process.stdout.on('data', (data) => {
+            stringData += data.toString();
+            
+        });
+
+        if (args) {
+            let data = [...args];
+            process.stdin.write(JSON.stringify(data));
+        }
+
+        process.stderr.on('data', (data) => {
+            reject(data.toString());
+        });
+
+       /* process.stdout.on('end', () => {
+            if (stringData !== '') {
+                resolve(stringData);
+            } else {
+                reject(-1);
+            }
+        }); */
+
+        process.on('close', (code) => {
+            if (code === 0) {
+                resolve(stringData.trim());
+            } else {
+                reject(`Python script exited with code ${code}`);
+            }
+        });
+
+        process.stdin.end();
     });
-    if (args) {
-        let data = [...args];
-        process.stdin.write(JSON.stringify(data));
-        process.stdout.on('end',() => {
-            stringData !== '' ? 1 : -1
-        })
-    } 
+};
 
-    process.stderr.on('data', (data) => {
-        return data
-    });
-
-    return stringData;
-}
-
-
+(async () => {
+    try {
+        const result = await pyProcess('./deneme.py');
+    } catch (error) {
+        console.error('Hata:', error);
+    }
+})();
